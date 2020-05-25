9b0a07dc9de52cf33c436dab02022b3a2707fd06
Pascal Baljet <pascal@protone.media>
2020-05-25 10:20:18 +0200

    Update ServiceProvider.php

--- src/Support/ServiceProvider.php
+++ src/Support/ServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace Pbmedia\LaravelFFMpeg\Support;
 
+use FFMpeg\Driver\FFMpegDriver;
 use FFMpeg\FFMpeg;
+use FFMpeg\FFProbe;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 use Pbmedia\LaravelFFMpeg\Drivers\PHPFFMpeg;
 use Pbmedia\LaravelFFMpeg\MediaOpener;
@@ -30,19 +32,46 @@ class ServiceProvider extends BaseServiceProvider
         // Automatically apply the package configuration
         $this->mergeConfigFrom(__DIR__ . '/../../config/config.php', 'laravel-ffmpeg');
 
-        $this->app->bind(PHPFFMpeg::class, function () {
-            $config = $this->app['config'];
-
-            $logger = $config->get('laravel-ffmpeg.enable_logging', true)
+        $this->app->singleton('laravel-ffmpeg-logger', function () {
+            return $this->app['config']->get('laravel-ffmpeg.enable_logging', true)
                 ? app(LoggerInterface::class)
                 : null;
+        });
 
-            return new PHPFFMpeg(FFMpeg::create([
+        $this->app->singleton('laravel-ffmpeg-configuration', function () {
+            $config = $this->app['config'];
+
+            return [
                 'ffmpeg.binaries'  => $config->get('laravel-ffmpeg.ffmpeg.binaries'),
                 'ffmpeg.threads'   => $config->get('laravel-ffmpeg.ffmpeg.threads', 12),
                 'ffprobe.binaries' => $config->get('laravel-ffmpeg.ffprobe.binaries'),
                 'timeout'          => $config->get('laravel-ffmpeg.timeout'),
-            ], $logger));
+            ];
+        });
+
+        $this->app->bind(FFProbe::class, function () {
+            return FFProbe::create(
+                $this->app->make('laravel-ffmpeg-configuration'),
+                $this->app->make('laravel-ffmpeg-logger')
+            );
+        });
+
+        $this->app->bind(FFMpegDriver::class, function () {
+            return FFMpegDriver::create(
+                $this->app->make('laravel-ffmpeg-logger'),
+                $this->app->make('laravel-ffmpeg-configuration')
+            );
+        });
+
+        $this->app->bind(FFMpeg::class, function () {
+            return new FFMpeg(
+                $this->app->make(FFMpegDriver::class),
+                $this->app->make(FFProbe::class)
+            );
+        });
+
+        $this->app->bind(PHPFFMpeg::class, function () {
+            return new PHPFFMpeg($this->app->make(FFMpeg::class));
         });
 
         // Register the main class to use with the facade
