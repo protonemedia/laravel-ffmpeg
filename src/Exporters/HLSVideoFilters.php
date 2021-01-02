380b34c88ec9b9d407eba19bf1af5b8282f8672f
pascalbaljet <pascal@pascalbaljet.nl>
2021-01-02 01:00:25 +0100

    WIP

--- src/Exporters/HLSVideoFilters.php
+++ src/Exporters/HLSVideoFilters.php
@@ -107,7 +107,7 @@ class HLSVideoFilters
     {
         $withWatermarkFactory($watermarkFactory = new WatermarkFactory);
 
-        return $this->addLegacyFilter($watermarkFactory->get());
+        return $this->addLegacyFilter($watermarkFactory->get()->wrapInParentheses());
     }
 
     /**
--- src/Filters/WatermarkFilter.php
+++ src/Filters/WatermarkFilter.php
@@ -11,6 +11,7 @@ use FFMpeg\Filters\Video\WatermarkFilter as FFMpegWatermarkFilter;
 class WatermarkFilter extends FFMpegWatermarkFilter
 {
     protected $path;
+    protected $wrapInParentheses = false;
 
     public function __construct($watermarkPath, array $coordinates = [], $priority = 0)
     {
@@ -19,24 +20,50 @@ class WatermarkFilter extends FFMpegWatermarkFilter
         $this->path = $watermarkPath;
     }
 
+    public function wrapInParentheses(): self
+    {
+        $this->wrapInParentheses = true;
+
+        return $this;
+    }
+
+    /**
+     * Gets the commands from the base filter and normalizes the path.
+     *
+     * @return array
+     */
     protected function getCommands()
     {
         $commands = parent::getCommands();
 
-        $commands[1] = str_replace(
-            $this->path,
-            static::normalizePath($this->path),
-            $commands[1]
-        );
+        $replace = static::normalizePath($this->path);
+
+        if ($this->wrapInParentheses) {
+            $replace = "'{$replace}'";
+        }
+
+        $commands[1] = str_replace($this->path, $replace, $commands[1]);
 
         return $commands;
     }
 
+    /**
+     * Normalizes the path when running on Windows.
+     *
+     * @param string $path
+     * @return string
+     */
     public static function normalizePath(string $path): string
     {
         return windows_os() ? static::normalizeWindowsPath($path) : $path;
     }
 
+    /**
+     * Replaces the slashes and escapes the colon.
+     *
+     * @param string $path
+     * @return string
+     */
     public static function normalizeWindowsPath(string $path): string
     {
         $path = str_replace('/', '\\', $path);
