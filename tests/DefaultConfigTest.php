12fcc552dd08e85fba5e13c589240474ed7a90a3
pascalbaljet <pascal@pascalbaljet.nl>
2022-02-22 17:08:47 +0100

    Update DefaultConfigTest.php

--- tests/DefaultConfigTest.php
+++ tests/DefaultConfigTest.php
@@ -2,7 +2,6 @@
 
 namespace ProtoneMedia\LaravelFFMpeg\Tests;
 
-use FFMpeg\Format\Video\X264;
 use ProtoneMedia\LaravelFFMpeg\Support\FFMpeg;
 
 class DefaultConfigTest extends TestCase
@@ -17,18 +16,5 @@ class DefaultConfigTest extends TestCase
             ->getCommand('test.mp4');
 
         $this->assertStringContainsString('-threads 1', $command[0]);
-    }
-
-    /** @test */
-    public function it_passes_the_default_temporary_root_to_the_underlying_driver()
-    {
-        $this->fakeLocalVideoFile();
-
-        $command = FFMpeg::open('video.mp4')
-            ->export()
-            ->inFormat(new X264)
-            ->getCommand('test.mp4');
-
-        $this->assertStringContainsString('-passlogfile ' . sys_get_temp_dir(), $command[0]);
     }
 }
