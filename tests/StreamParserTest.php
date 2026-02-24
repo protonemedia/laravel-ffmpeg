59559c631fdbbde1a4b1a9b572283397d345ce20
realpascalbotjet <realpascalbotjet@protonmail.com>
2026-02-24 22:16:38 +0100

    Fix: Add validation for framerate parsing in StreamParser (#567)
    Co-authored-by: Pascal Baljet <pascal@protone.media>

--- tests/StreamParserTest.php
+++ tests/StreamParserTest.php
@@ -16,4 +16,14 @@ class StreamParserTest extends TestCase
         $this->assertEquals(25, StreamParser::new(new Stream(['avg_frame_rate' => '250/10']))->getFrameRate());
         $this->assertEquals(25, StreamParser::new(new Stream(['avg_frame_rate' => '50/2']))->getFrameRate());
     }
+
+    #[Test]
+    /** @test */
+    public function it_returns_null_for_invalid_average_frame_rate_values()
+    {
+        $this->assertNull(StreamParser::new(new Stream(['avg_frame_rate' => '']))->getFrameRate());
+        $this->assertNull(StreamParser::new(new Stream(['avg_frame_rate' => 'foo/bar']))->getFrameRate());
+        $this->assertNull(StreamParser::new(new Stream(['avg_frame_rate' => '25/1/2']))->getFrameRate());
+        $this->assertNull(StreamParser::new(new Stream(['avg_frame_rate' => '25/0']))->getFrameRate());
+    }
 }
